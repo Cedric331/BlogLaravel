63d9bfc4419d02f97d43689a9c1945d64ed6c8e3
Cedric Lima <limacedric@hotmail.fr>
2021-03-07 14:57:29 +0100

    Init project

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <layout title="Welcome">
+    <h1>Welcome</h1>
+    <p>Hello , welcome to your first Inertia app!</p>
+  </layout>
+</template>
+
+<script>
+
+
+  export default {
+
+    props: {
+      user: Object,
+    },
+  }
+</script>
